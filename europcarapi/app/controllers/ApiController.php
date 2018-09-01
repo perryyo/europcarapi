2bdbfa6824a82bea2e93c9c77aabddf8462ac9e2
Matainja Technologies <identity.self@gmail.com>
2018-09-01 12:22:33 +0530

    PSR-2 standards

--- europcarapi/app/controllers/ApiController.php
+++ europcarapi/app/controllers/ApiController.php
@@ -1,14 +1,21 @@
 <?php
-use App\Libraries\Utils;
+
+	use App\Libraries\Utils;
+
 class ApiController extends ControllerBase
 {
-    public function indexAction()
-    {
 
-    }
-	
-	/* Method : Getting Error Response */
-	public static function geterror($key='',$str){
+	/**
+	  * Getting error response for missing parameters.
+	  *
+	  * @param string $str represents name of the attribute.
+	  * @param null or string  $key represents null if all parameters missing or an attribute as key.
+	  *
+	  * @return error response with code.
+	*/
+
+	public function error($str, $key = null)
+	{
 		$attr = 'attributes';
 		$element = new stdClass;	
 		$element->serviceResponse =  new stdClass;
@@ -16,30 +23,48 @@ class ApiController extends ControllerBase
 		$flag = 'xrs.missing.'.$str.$key;
 		$element->serviceResponse->$attr = array('errorCode'=>$flag,'returnCode' => 'KO');
 		return $element;
-	 }
-	 
-	 /* Method : Conversion from xml to array */
-	 public function xml2array ( $xmlObject, $out = array () )
-	 {
-		foreach ( (array) $xmlObject as $index => $node )
-			$out[$index] = ( is_object ( $node ) ) ? $this->xml2array ( $node ) : $node;
-	
+	}
+
+	/**
+	  * Conversion from xml to array.
+	  *
+	  * @param object $xmlObject represents xml object.
+	  * @param array  $out represents converted array from object.
+	  *
+	  * @return array.
+	*/
+
+	public function xml2array($xmlObject, $out = array())
+	{
+		foreach ((array)$xmlObject as $index => $node){
+			$out[$index] = (is_object($node)) ? $this->xml2array($node) : $node;
+		}
 		return $out;
-	 }
-	 
-	 /* Method : Replace Array Keys */
-	 public function replaceKey($subject, $newKey, $oldKey) {
-		 
+	}
+
+	/**
+	  * Replace array keys.
+	  *
+	  * @param array $xmlObject represents array whose keys need to be replaced.
+	  * @param string  $newKey represents key which is to be replaced with.
+	  * @param string  $oldKey represents key which need to be replaced.
+	  *
+	  * @return array.
+	*/
+
+	public function replaceKey($subject, $newKey, $oldKey) 
+	{
 		if (!is_array($subject)) return $subject;
-	
+
 		$newArray = array(); // empty array to hold copy of subject
 		foreach ($subject as $key => $value) {
 			// replace the key with the new key only if it is the old key
 			$key = ($key === $oldKey) ? $newKey : $key;
-			if(is_array($subject[$key])){
-				if(!empty($subject[$key])){
-					if(count($subject[$key]) > 1){
-						foreach($subject[$key] as $k => $v){
+
+			if (is_array($subject[$key])){
+				if (!empty($subject[$key])){
+					if (count($subject[$key]) > 1){
+						foreach ($subject[$key] as $k => $v){
 							$k = ($k === $oldKey) ? $newKey : $k;
 							$v = (is_array($v)) ? $v : $this->xml2array($v);
 							$newArray[$key][$k] = $this->replaceKey($v, $newKey, $oldKey);
@@ -49,7 +74,7 @@ class ApiController extends ControllerBase
 						$newArray[$key] = $this->replaceKey($value, $newKey, $oldKey); 
 					}
 				}
-				
+
 			}else{
 				$newArray[$key] = $this->replaceKey($value, $newKey, $oldKey);
 			}
@@ -57,116 +82,149 @@ class ApiController extends ControllerBase
 		}
 		return $newArray;
 	}
-	 
-	/* Method : Get Countries list with country code */
-    public function getCountriesAction()
-    {
-        $input_xml = ' ';   
+
+	/**
+	  * Get list of countries with country code and country description.
+	  *
+	  *
+	  * @return json.
+	*/
+
+	public function getCountriesAction()
+	{
 		$url = Utils::countryservice();
-		$data = Utils::curl($url, $input_xml);
+		$data = Utils::curl($url);
 		$xmlArr = $this->xml2array($data);
-		$arr = $this->replaceKey($xmlArr,'attributes','@attributes');
-		
+		$arr = $this->replaceKey($xmlArr, 'attributes', '@attributes');
+
 		header('Content-Type: application/json');
 		echo json_encode($arr);
-    }
-	
-	/* Method : Get Cities list wrt country code */
+	}
+
+	/**
+	  * Get list of cities with city code and city description.
+	  *
+	  * @param string countryCode, param need to be passed, as for example { countryCode : AE }.
+	  *
+	  * @return json.
+	*/
+
 	public function getCitiesAction()
-    {
-		/* Parameters : { countryCode : AE } */
-		$input_xml = ' ';   
+	{
 		$params = $this->request->getPost();
-		if(isset($params) && !empty($params)){	
-			if(isset($params['countryCode']) && $params['countryCode']!=''){
+		if (isset($params) && !empty($params)){	
+			if(isset($params['countryCode']) && $params['countryCode'] != ''){
 				$url = Utils::cityservice($params['countryCode']);
-				$xml = Utils::curl($url, $input_xml);
+				$xml = Utils::curl($url);
 			}else{
-				$xml = $this->geterror('key','country');
+				$xml = $this->error('country', 'key');
 			}
-		}else{
-			$xml = $this->geterror('','parameters');
+		} else {
+			$xml = $this->error('parameters');
 		}
 		$xmlArr = $this->xml2array($xml);
-		$arr = $this->replaceKey($xmlArr,'attributes','@attributes');
+		$arr = $this->replaceKey($xmlArr, 'attributes', '@attributes');
 
 		header('Content-Type: application/json');
 		echo json_encode($arr);
-    }
-	
-	/* Method : Get Stations list wrt country code & city name */
+	}
+
+	/**
+	  * Get list of stations with station code and station name.
+	  *
+	  * @param string countryCode, param need to be passed, as for example { countryCode : AE }.
+	  * @param string cityName, param need to be passed, as for example { cityName : ABU DHABI }.
+	  *
+	  * @return json.
+	*/
+
 	public function getStationsAction()
-    {
-		/* Parameters : { countryCode : AE , cityName : ABU DHABI } */
-		$input_xml = ' ';   
+	{
 		$params = $this->request->getPost();
-		if(isset($params) && !empty($params)){	
-			if((isset($params['countryCode']) && $params['countryCode']!='') && (isset($params['cityName']) && $params['cityName']!='')){
+		if (isset($params) && !empty($params)){	
+			if((isset($params['countryCode']) && $params['countryCode'] != '') && (isset($params['cityName']) && $params['cityName'] != '')){
 				$url = Utils::stationservice($params);
-				$xml = Utils::curl($url, $input_xml);
-			}else{
-				if(!isset($params['countryCode']) || $params['countryCode'] == ''){
-					$xml = $this->geterror('key','country');
+				$xml = Utils::curl($url);
+			} else {
+				if (!isset($params['countryCode']) || $params['countryCode'] == ''){
+					$xml = $this->error('country','key');
 				}
-				if(!isset($params['cityName']) || $params['cityName'] == ''){
-					$xml = $this->geterror('key','city');
+				if (!isset($params['cityName']) || $params['cityName'] == ''){
+					$xml = $this->error('city','key');
 				}
 			}
-		}else{
-			$xml = $this->geterror('','parameters');
+		} else {
+			$xml = $this->error('','parameters');
 		}
 		$xmlArr = $this->xml2array($xml);
-		$arr = $this->replaceKey($xmlArr,'attributes','@attributes');
+		$arr = $this->replaceKey($xmlArr, 'attributes', '@attributes');
 
 		header('Content-Type: application/json');
 		echo json_encode($arr);
-    }
-	
+	}
+
 	/* Method : Get Car categories list wrt station code and date */
+	/**
+	  * Get list of car categories with related information.
+	  *
+	  * @param string stationID, param need to be passed, as for example { stationID : MSQR02 }.
+	  * @param date format (Ymd) date, param need to be passed, as for example { date : 20180829 }.
+	  *
+	  * @return json.
+	*/
+
 	public function getCarCategoriesAction()
-    {
-		/* Parameters : { stationID : MSQR02 , date : 20180829 } */
-		$input_xml = ' ';   
+	{
 		$params = $this->request->getPost();
-		if(isset($params) && !empty($params)){
-			if((isset($params['stationID']) && $params['stationID']!='') && (isset($params['date']) && $params['date']!='')){
+		if (isset($params) && !empty($params)){
+			if ((isset($params['stationID']) && $params['stationID']!='') && (isset($params['date']) && $params['date']!='')){
 				//Date in Ymd format
 				$url = Utils::carcatservice($params);
-				$xml = Utils::curl($url, $input_xml);
-			}else{
-				if(!isset($params['stationID']) || $params['stationID'] == ''){
-					$xml = $this->geterror('key','stationcode');
+				$xml = Utils::curl($url);
+			} else {
+				if (!isset($params['stationID']) || $params['stationID'] == ''){
+					$xml = $this->error('stationcode','key');
 				}
-				if(!isset($params['date']) || $params['date'] == ''){
-					$xml = $this->geterror('key','date');
+				if (!isset($params['date']) || $params['date'] == ''){
+					$xml = $this->error('date','key');
 				}
 			}
-		}else{
-			$xml = $this->geterror('','parameters');		
+		} else {
+			$xml = $this->error('parameters');		
 		}
-		
+
 		$xmlArr = $this->xml2array($xml);
-		$arr = $this->replaceKey($xmlArr,'attributes','@attributes');
+		$arr = $this->replaceKey($xmlArr, 'attributes', '@attributes');
 
 		header('Content-Type: application/json');
 		echo json_encode($arr);
-    }
-	
-	/* Method : Get quotation list  */
+	}
+
+	/**
+	  * Get list of car quotations with related information.
+	  *
+	  * @param string carCategory, param need to be passed, as for example { carCategory : IDAR }.
+	  * @param string checkout_stationID, param need to be passed, as for example { checkout_stationID : MSQR02 }.
+	  * @param string checkin_stationID, param need to be passed, as for example { checkin_stationID : MSQR02 }.
+	  * @param datetime format (d/m/Y H:i) checkout_time, param need to be passed, as for example { checkout_time : 01/09/2018 10:00 }.
+	  * @param datetime format (d/m/Y H:i) checkin_time, param need to be passed, as for example { checkin_time : 31/08/2018 23:00 }.
+	  * @param string countryCode, param need to be passed, as for example { countryCode : AE }.
+	  *
+	  * @return json.
+	*/
+
 	public function getQuoteAction()
-    {
-		/* Parameters : { carCategory : IDAR, checkout_stationID: MSQR02, checkin_stationID : MSQR02, checkout_time : 30/08/2018 10:00, checkin_time : 29/08/2018 23:00, countryCode : AE } */
-		$input_xml = ' ';  
+	{
 		$flag = ''; 
 		$params = $this->request->getPost();
-		if(isset($params) && !empty($params)){
+		if (isset($params) && !empty($params)){
 			$carCategory =  isset($params['carCategory']) ? $params['carCategory'] : '';
 			$checkin_stationID = 	isset($params['checkin_stationID']) ? $params['checkin_stationID'] : '';
 			$checkout_stationID = 	isset($params['checkout_stationID']) ? $params['checkout_stationID'] : '';
 			$checkin_time =  isset($params['checkin_time']) ? $params['checkin_time'] : '';
 			$checkout_time =  isset($params['checkout_time']) ? $params['checkout_time'] : '';
 			$countryCode =  isset($params['countryCode']) ? $params['countryCode'] : '';
-			
+
 			$checkParamArr = array(
 				'carCategory' => $carCategory,
 				'checkin.stationID' => $checkin_stationID,
@@ -175,8 +233,8 @@ class ApiController extends ControllerBase
 				'checkout.time' => $checkout_time,
 				'countryCode' => $countryCode
 			);
-			
-			if(isset($checkParamArr) && !empty($checkParamArr)){
+
+			if (isset($checkParamArr) && !empty($checkParamArr)){
 				foreach ($checkParamArr as $key => $value) {
 					$value = trim($value);
 					if ($value == ''){
@@ -188,52 +246,52 @@ class ApiController extends ControllerBase
 					}
 				}
 			}
-			
-			if($flag!=''){
-				$attr = '@attributes';
+
+			if ($flag != ''){
+				$attr = 'attributes';
 				$element = new stdClass;
 				$element->serviceResponse =  new stdClass;
-				$element->serviceResponse->$attr = array('errorCode'=>$flag,'returnCode' => 'KO');
+				$element->serviceResponse->$attr = array('errorCode' => $flag,'returnCode' => 'KO');
 				$xml = $element;
-			}else{
-				if($checkin_time!=''){
-					$newDate = DateTime::createFromFormat("d/m/Y H:i",$checkin_time);
+			} else {
+				if ($checkin_time != ''){
+					$newDate = DateTime::createFromFormat("d/m/Y H:i", $checkin_time);
 					$check_in_date = $newDate->format('Ymd'); 
 					$check_in_time = $newDate->format('Hi'); 
-				}else{
+				} else {
 					$check_in_date = $check_in_time =  '';
 				}
-				
-				if($checkout_time!=''){
-					$newDate = DateTime::createFromFormat("d/m/Y H:i",$checkout_time);
+
+				if($checkout_time != ''){
+					$newDate = DateTime::createFromFormat("d/m/Y H:i", $checkout_time);
 					$check_out_date = $newDate->format('Ymd'); 
 					$check_out_time = $newDate->format('Hi'); 
 				}else{
 					$check_out_date = $check_out_time =  '';
 				}
-				
+
 				$paramArr['carCategory'] = $carCategory;
 				$paramArr['checkout_stationID'] = $checkout_stationID;
 				$paramArr['check_in_date'] = $check_in_date;
 				$paramArr['check_in_time'] = $check_in_time;
 				$paramArr['checkin_stationID'] = $checkin_stationID;
-				
+
 				$paramArr['check_out_date'] = $check_out_date;
 				$paramArr['check_out_time'] = $check_out_time;
 				$paramArr['countryCode'] = $countryCode;
-				
+
 				$url = Utils::quoteservice($paramArr);
-				$xml = Utils::curl($url, $input_xml);
+				$xml = Utils::curl($url);
 			}
 		}else{
-			$xml = $this->geterror('','parameters');
+			$xml = $this->error('parameters');
 		}
-		
+
 		$xmlArr = $this->xml2array($xml);
-		$arr = $this->replaceKey($xmlArr,'attributes','@attributes');
+		$arr = $this->replaceKey($xmlArr, 'attributes', '@attributes');
 
 		header('Content-Type: application/json');
 		echo json_encode($arr);
-    }
-	
+	}
+
 }
